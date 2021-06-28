6bd8623611ac6f9ddfdcf101e6f833c8affdb47e
Francisco Fleury <franmeifleury@gmail.com>
2021-06-28 12:19:49 -0300

    teste

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ def transDever(lista):
     for key, i in lista.items():
         newNumber = int(i['dataEnd'][0:2]) + (int(i['dataEnd'][3:5]) * 100) + (int(i['dataEnd'][6:10])* 10000)
         print(newNumber)
-        i['dataValue'] = newNumber
+        key['dataValue'] = newNumber
+    print(lista)
     return lista
 @client.command(name = 'clear')
 async def limpar(context):
